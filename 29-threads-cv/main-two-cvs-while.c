5b2af59bba3ffa52f4e96bcdd7d715ac3c5adead
modulitos <modulitos@users.noreply.github.com>
2020-12-25 19:41:20 -0800

    update makefile

--- 29-threads-cv/main-two-cvs-while.c
+++ 29-threads-cv/main-two-cvs-while.c
@@ -30,7 +30,7 @@ void do_fill(int value) {
 int do_get() {
     int tmp = buffer[use_ptr];
     ensure(tmp != EMPTY, "error: tried to get an empty buffer");
-    buffer[use_ptr] = EMPTY; 
+    buffer[use_ptr] = EMPTY;
     use_ptr = (use_ptr + 1) % max;
     num_full--;
     return tmp;
@@ -39,7 +39,7 @@ int do_get() {
 void *producer(void *arg) {
     int id = (int) arg;
     // make sure each producer produces unique values
-    int base = id * loops; 
+    int base = id * loops;
     int i;
     for (i = 0; i < loops; i++) {   p0;
 	Mutex_lock(&m);             p1;
@@ -52,7 +52,7 @@ void *producer(void *arg) {
     }
     return NULL;
 }
-                                                                               
+
 void *consumer(void *arg) {
     int id = (int) arg;
     int tmp = 0;
