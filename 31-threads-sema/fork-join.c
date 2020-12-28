6ae981f1b31f78a96444e6fe4d06a710b145105a
modulitos <modulitos@users.noreply.github.com>
2020-12-27 22:52:59 -0800

    31: rendezvous; reformat

--- 31-threads-sema/fork-join.c
+++ 31-threads-sema/fork-join.c
@@ -8,21 +8,21 @@
 Zem_t s;
 
 void *child(void *arg) {
-  sleep(3);
-  printf("child\n");
-  // use semaphore here
-  Zem_post(&s);
-  return NULL;
+    sleep(3);
+    printf("child\n");
+    // use semaphore here
+    Zem_post(&s);
+    return NULL;
 }
 
 int main(int argc, char *argv[]) {
-  pthread_t p;
-  printf("parent: begin\n");
-  // init semaphore here
-  Zem_Init(&s, 0);
-  Pthread_create(&p, NULL, child, NULL);
-  // use semaphore here
-  Zem_wait(&s);
-  printf("parent: end\n");
-  return 0;
+    pthread_t p;
+    printf("parent: begin\n");
+    // init semaphore here
+    Zem_Init(&s, 0);
+    Pthread_create(&p, NULL, child, NULL);
+    // use semaphore here
+    Zem_wait(&s);
+    printf("parent: end\n");
+    return 0;
 }
